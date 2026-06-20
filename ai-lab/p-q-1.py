5498ffde01fe35b36415ccf7095173c20ed27f5b
mrfhmd7 <mrf.hmd7@gmail.com>
2026-06-20 17:30:15 +0600

    first

---
+++ ai-lab/p-q-1.py
@@ -0,0 +1,7 @@
+# Experiment No: 01
+# Experiment Name: Write a program using Python for addition and multiplication of two numbers.
+# Source Code:
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("Addition =", a + b)
+print("Multiplication =", a * b)
