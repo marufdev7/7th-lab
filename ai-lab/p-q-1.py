4009b8da03018cb4a751926b7d8a1d541ce413b6
mrfhmd7 <mrf.hmd7@gmail.com>
2026-06-20 17:32:04 +0600

    output add

--- ai-lab/p-q-1.py
+++ ai-lab/p-q-1.py
@@ -5,3 +5,8 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 print("Addition =", a + b)
 print("Multiplication =", a * b)
+
+# Enter first number: 5
+# Enter second number: 15
+# Addition = 20
+# Multiplication = 75
